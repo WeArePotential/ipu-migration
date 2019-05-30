713945fcc4968e19b46e9616c6874255911c89d5
Peter Mason <pmasonids@gmail.com>
2019-05-30 18:14:40 +0100

    Export config, move language switcher, try field_group again

--- web/sereno/script/d7_export (to run in D7 site)/export_data_ALL_field_sections.php
+++ web/sereno/script/d7_export (to run in D7 site)/export_data_ALL_field_sections.php
@@ -12,14 +12,12 @@ $errors = [];
 foreach ($result as $obj) {
   $nids[] = $obj->nid;
   $nid = $obj->nid;
-  //if ($nid == 236) {
 
   $output .= "\n\r" . ' $d8_nodes[' . $nid . '] = array(';
   //  print "\n\r";
   //  print "*********************************";
   //  print "\n\r";
   //  print $nid;
-
 
   $tally++;
 
@@ -50,145 +48,141 @@ foreach ($result as $obj) {
     else {
       $errors[] = 'Node ' . $nid . ' only has English FCs';
     }
-  } else {
+  }
+  else {
     if (is_array($french_items)) {
       $errors[] = 'Node ' . $nid . ' only has French FCs';
     }
   }
 
-    //debug($english_items);
-    //debug($french_items);
+  //debug($english_items);
+  //debug($french_items);
 
-    //here items are having the entity id/itemid
+  //here items are having the entity id/itemid
 
-    // $field_info = field_info_field($items);
-    // print_r("field_info");
-    // print_r($field_info);
-    //$lang = $field_info['translatable'] ? entity_language('field_collection', $fc) : LANGUAGE_NONE;
+  // $field_info = field_info_field($items);
+  // print_r("field_info");
+  // print_r($field_info);
+  //$lang = $field_info['translatable'] ? entity_language('field_collection', $fc) : LANGUAGE_NONE;
 
-    #echo '>>'.$lang;
+  #echo '>>'.$lang;
 
-    if (empty($items)) {
-      continue;
+  if (empty($items)) {
+    //continue;
+  }
+
+  foreach ($items as $item) {
+    //debug($item);
+
+    $fc = field_collection_field_get_entity($item); // Do something.
+    //debug($fc);
+
+
+    // Start item
+    $output .= "\r" . '  array(';
+
+    $output .= "\r" . '    "field_original_fc_id"=>"' . $item['value'] . '", ';
+    $output .= "\r" . '    "field_original_fc_revision_id"=>"' . $item['revision_id'] . '", ';
+    $output .= "\r" . '    "field_original_fc_langcode"=>"' . $item['langcode'] . '", ';
+
+    if (!empty($fc->field_section_title)) {
+      foreach ($fc->field_section_title as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_section_title"=>"' . htmlspecialchars($content['value'], ENT_QUOTES) . '", ';
+          }
+        }
+      }
     }
 
-    foreach ($items as $item) {
-      //debug($item);
-
-      $fc = field_collection_field_get_entity($item); // Do something.
-      //debug($fc);
-
-
-      $output .= "\r" . '  array(';
-      $output .= "\r" . '    "field_original_fc_id"=>"' . $item['value'] . '", ';
-      $output .= "\r" . '    "field_original_fc_revision_id"=>"' . $item['revision_id'] . '", ';
-      $output .= "\r" . '    "field_original_fc_langcode"=>"' . $item['langcode'] . '", ';
-
-      if (!empty($fc->field_section_title)) {
-        foreach ($fc->field_section_title as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_section_title"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
-            else {
-              continue;
-            }
+    if (!empty($fc->field_section_description)) {
+      foreach ($fc->field_section_description as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_section_description"=>"' . htmlspecialchars($content['value'], ENT_QUOTES) . '", ';
           }
         }
       }
-      else {
-        continue;
-      }
-
-      if (!empty($fc->field_section_description)) {
-        foreach ($fc->field_section_description as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_section_description"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_display_options)) {
-        foreach ($fc->field_section_display_options as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '"field_section_display_options"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_html)) {
-        foreach ($fc->field_section_html as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_section_html"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_node)) {
-        foreach ($fc->field_section_node as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_section_node"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_view)) {
-        foreach ($fc->field_section_view as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['vname'])) {
-              $output .= "\r" . '    "field_section_view_vname"=>"' . htmlspecialchars($content['vname']) . '", ';
-            }
-            if (!empty($content['vdisplay'])) {
-              $output .= "\r" . '    "field_section_view_vdisplay"=>"' . htmlspecialchars($content['vdisplay']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_view)) {
-        foreach ($fc->field_section_view as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['vargs'])) {
-              $output .= "\r" . '    "field_section_view_vargs"=>"' . htmlspecialchars($content['vargs']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_view_arguments)) {
-        foreach ($fc->field_section_view_arguments as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['target_id'])) {
-              $output .= "\r" . '    "field_section_view_arguments"=>"' . htmlspecialchars($content['target_id']) . '", ';
-            }
-          }
-        }
-      }
-
-      if (!empty($fc->field_section_block)) {
-        foreach ($fc->field_section_block as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_section_block"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
-          }
-        }
-      }
-
-      $output .= "\r  ),";
     }
 
-    $output .= "\r );";
-  //}
-}
+    if (!empty($fc->field_section_display_options)) {
+      foreach ($fc->field_section_display_options as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '"field_section_display_options"=>"' . htmlspecialchars($content['value']) . '", ';
+          }
+        }
+      }
+    }
+
+    if (!empty($fc->field_section_html)) {
+      foreach ($fc->field_section_html as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_section_html"=>"' . htmlspecialchars($content['value'], ENT_QUOTES) . '", ';
+          }
+        }
+      }
+    }
+
+    if (!empty($fc->field_section_node)) {
+      foreach ($fc->field_section_node as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_section_node"=>"' . htmlspecialchars($content['value']) . '", ';
+          }
+        }
+      }
+    }
+
+    if (!empty($fc->field_section_view)) {
+      foreach ($fc->field_section_view as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['vname'])) {
+            $output .= "\r" . '    "field_section_view_vname"=>"' . htmlspecialchars($content['vname']) . '", ';
+          }
+          if (!empty($content['vdisplay'])) {
+            $output .= "\r" . '    "field_section_view_vdisplay"=>"' . htmlspecialchars($content['vdisplay']) . '", ';
+          }
+        }
+      }
+    }
+
+    if (!empty($fc->field_section_view)) {
+      foreach ($fc->field_section_view as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['vargs'])) {
+            $output .= "\r" . '    "field_section_view_vargs"=>"' . htmlspecialchars($content['vargs']) . '", ';
+          }
+        }
+      }
+    }
+
+    if (!empty($fc->field_section_view_arguments)) {
+      foreach ($fc->field_section_view_arguments as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['target_id'])) {
+            $output .= "\r" . '    "field_section_view_arguments"=>"' . htmlspecialchars($content['target_id']) . '", ';
+          }
+        }
+      }
+    }
+
+    if (!empty($fc->field_section_block)) {
+      foreach ($fc->field_section_block as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_section_block"=>"' . htmlspecialchars($content['value']) . '", ';
+          }
+        }
+      }
+    }
+
+    // end of all fields
+    $output .= "\r  ),";
+  }
+  $output .= "\r );";
+} // End foreach node
 
 //debug($counts);
 debug($output);
--- web/sereno/script/d7_export (to run in D7 site)/export_data_fc_field_ipu_event_document.php
+++ web/sereno/script/d7_export (to run in D7 site)/export_data_fc_field_ipu_event_document.php
@@ -15,65 +15,71 @@ foreach ($result as $obj) {
   $id = $obj->item_id;
 
   $tally++;
+  if ($id == 5314) {
+    $fc = entity_load_single('field_collection_item', $id, 'en');
+    debug ($fc->field_event_document_description);
+    $fc = entity_load_single('field_collection_item', $id, 'fr');
+    debug ($fc->field_event_document_description);
 
-  $fc = entity_load_single('field_collection_item', $id);
+    // "field_event_document_description"=>array("value"=>"&lt;p&gt;The involvement and active commitment of parliaments in maintaining international security and peace through support for a political solution&lt;/p&gt;", "format"=>"full_html"),
+    // "field_event_document_title"=>"Programme",
+    // "field_speech"=>0,
+    // "field_event_document"=>6270,
+    // "field_original_fc_id"=>5314,
 
-// "field_event_document_description"=>array("value"=>"&lt;p&gt;The involvement and active commitment of parliaments in maintaining international security and peace through support for a political solution&lt;/p&gt;", "format"=>"full_html"),
-// "field_event_document_title"=>"Programme",
-// "field_speech"=>0,
-// "field_event_document"=>6270,
-// "field_original_fc_id"=>5314,
-
-//  echo 'lang';
-//  echo entity_language('field_collection_item', $fc);
+    //  echo 'lang';
+    //  echo entity_language('field_collection_item', $fc);
 
 
-      $output .= "\r\r" . '$field_ipu_event_document[' . $id . '] = array(';
+    $output .= "\r\r" . '$field_ipu_event_document[' . $id . '] = array(';
 
-      if (!empty($fc->field_event_document_title)) {
-        foreach ($fc->field_event_document_title as $contents) {
-          foreach ($contents as $content) {
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_event_document_title"=>"' . htmlspecialchars($content['value']) . '", ';
-            }
+    if (!empty($fc->field_event_document_title)) {
+      foreach ($fc->field_event_document_title as $contents) {
+        foreach ($contents as $content) {
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_event_document_title"=>"' . htmlspecialchars($content['value']) . '", ';
           }
         }
       }
+    }
 
-      if (isset($fc->field_speech['und']['0']['value']) && $fc->field_speech['und']['0']['value'] == 1) {
-        $output .= "\r" . '    "field_speech"=>1,';
-      } else {
-        $output .= "\r" . '    "field_speech"=>0,';
-      }
+    if (isset($fc->field_speech['und']['0']['value']) && $fc->field_speech['und']['0']['value'] == 1) {
+      $output .= "\r" . '    "field_speech"=>1,';
+    }
+    else {
+      $output .= "\r" . '    "field_speech"=>0,';
+    }
 
-      if (!empty($fc->field_event_document_description)) {
-        foreach ($fc->field_event_document_description as $contents) {
-          debug($contents);
-          foreach ($contents as $content) {
-            debug($content);
-            if (!empty($content['value'])) {
-              $output .= "\r" . '    "field_event_document_description"=>array("value"=>"' . htmlspecialchars(trim($content['value'])) . '", "format"=>"' . ($content['format']) . '"), ';
-            }
+    if (!empty($fc->field_event_document_description)) {
+      foreach ($fc->field_event_document_description as $contents) {
+        debug($contents);
+        foreach ($contents as $content) {
+          debug($content);
+          if (!empty($content['value'])) {
+            $output .= "\r" . '    "field_event_document_description"=>array("value"=>"' . htmlspecialchars(trim($content['value'])) . '", "format"=>"' . ($content['format']) . '"), ';
           }
         }
       }
+    }
 
-      if (!empty($fc->field_event_document)) {
-        foreach ($fc->field_event_document as $contents) {
-          foreach ($contents as $content) {
-            # print "\n\r";
-            #debug($content);
-            # print(' - fid:' . $content['fid']);
-            if(!empty($content['fid']) && is_numeric($content['fid'])) {
-              $output .= "\r" . '    "field_event_document"=>' . $content['fid'] . ', ';
-            }
+    if (!empty($fc->field_event_document)) {
+      foreach ($fc->field_event_document as $contents) {
+        foreach ($contents as $content) {
+          # print "\n\r";
+          #debug($content);
+          # print(' - fid:' . $content['fid']);
+          if (!empty($content['fid']) && is_numeric($content['fid'])) {
+            $output .= "\r" . '    "field_event_document"=>' . $content['fid'] . ', ';
           }
         }
       }
+    }
 
-      $output .= "\r" . '    "field_original_fc_id"=>' . $id . ', ';
+    $output .= "\r" . '    "field_original_fc_id"=>' . $id . ', ';
 
-      $output .= "\r);";
+    $output .= "\r);";
+  } // Only get ID 2019
+
 }
 
 debug( $output );
