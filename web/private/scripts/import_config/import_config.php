d584b92617ac897165f8a147ed505c64729f23c5
Peter Mason <pmasonids@gmail.com>
2019-06-03 17:10:48 +0100

    New ipu_tweaks module

--- web/private/scripts/import_config/import_config.php
+++ web/private/scripts/import_config/import_config.php
@@ -38,9 +38,9 @@ if (defined('PANTHEON_ENVIRONMENT')) {
 
 
 if (!empty($config_directory)) {
-  //passthru("drush cim --partial --source=$config_directory --yes");
+  passthru("drush cim --partial --source=$config_directory --yes");
     //passthru('drush cset "system.site" uuid "e68855ce-257f-41af-9a61-51f6fc1116ef"');
-  passthru('drush cim  --yes');
+  //passthru('drush cim  --yes');
   passthru('drush cr');
 }
 
