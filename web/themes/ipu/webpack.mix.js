234174e8390bd9cd598522de2e984c78b3e981c2
Martin Edwards <mail@mledwards.co.uk>
2019-06-25 15:03:21 +0100

    Using ipu.site

--- web/themes/ipu/webpack.mix.js
+++ web/themes/ipu/webpack.mix.js
@@ -7,7 +7,7 @@
  | for your application. See https://github.com/JeffreyWay/laravel-mix.
  |
  */
-const proxy = 'http://ipu.local';
+const proxy = 'http://ipu.site';
 const mix = require('laravel-mix');
 
 /*
